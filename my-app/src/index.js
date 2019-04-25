5af2eb78e81e0d36e172e4dfe1b3f2b9fde22918
Jiong Liu <jonprivate1991@hotmail.com>
2019-04-24 23:29:49 -0700

    Start to use Modal and materialize.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,31 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './materialize/css/materialize.min.css';
+import M from 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.cards = new Array();
+    }
+
+    insertCard(card) {
+        this.cards.push(card);
+    }
+
     render() {
-        var cards = new Array();
-        cards.push(<Card
-            title="Card Title"
-            content="This is a dummy content that has no specific meaning. Use this as the default content of the card."
-            actions={[
-                <Action content = "Here's the 1st link"/>,
-                <Action content = "Here's the 2nd link"/>]} />);
-        cards.push(<Card
-            title="Card Title"
-            content="This is a dummy content that has no specific meaning. Use this as the default content of the card."
-            actions={[
-                <Action content = "Here's the 1st link"/>,
-                <Action content = "Here's the 2nd link"/>]} />);
-        const deck = <Deck cards={cards} />;
-        cards.push(<Card
-            title="Card Title"
-            content="This is a dummy content that has no specific meaning. Use this as the default content of the card."
-            actions={[
-                <Action content = "Here's the 1st link"/>,
-                <Action content = "Here's the 2nd link"/>]} />);
-        return <div class="container">{deck}</div>;
+        const deck = <Deck cards={this.cards} />;
+        const card = <Card
+        title="Card Title"
+        content="This is a dummy content that has no specific meaning. Use this as the default content of the card."
+        actions={[
+            <Action content="Here's the 1st link" />,
+            <Action content="Here's the 2nd link" />]} />;
+        return (
+            <div class="container">
+                {deck}
+                <ModalTrigger href="#modal1" content="Modal" />
+                <Modal id="modal1" content={card} footerContent="Agree" />
+            </div>
+        );
     }
 }
 
@@ -41,10 +44,13 @@ class Deck extends React.Component {
 
 class Card extends React.Component {
     render() {
+        var width = this.props.width ? this.props.width : "col s12 m6 l4";
+        var backColor = this.props.backColor ? this.props.backColor : "blue-grey darken-1";
+        var contentColor = this.props.contentColor ? this.props.contentColor : "white-text";
         return (
-            <div class="col s4 m4 l3">
-                <div class="card blue-grey darken-1">
-                    <div class="card-content white-text">
+            <div class={width}>
+                <div class={"card " + backColor}>
+                    <div class={"card-content " + contentColor}>
                         <span class="card-title">{this.props.title}</span>
                         <p>{this.props.content}</p>
                     </div>
@@ -59,20 +65,47 @@ class Card extends React.Component {
 
 class Action extends React.Component {
     render() {
-        var href = "#";
-        if (this.props.href) {
-            href = this.props.href;
-        }
-        var content = "action";
-        if (this.props.content) {
-            content = this.props.content;
-        }
+        var href = this.props.href ? this.props.href : "#";
+        var content = this.props.content ? this.props.content : "action";
         return (
             <a href={href}>{content}</a>
         );
     }
 }
 
-// ========================================
+class ModalTrigger extends React.Component {
+    render() {
+        return (
+            <a class="waves-effect waves-light btn modal-trigger" href={this.props.href}>{this.props.content}</a>
+        );
+    }
+}
 
-ReactDOM.render(<Board />, document.getElementById("materialize"));
+class Modal extends React.Component {
+    render() {
+        return (
+            <div id={this.props.id} class="modal">
+                <div class="modal-content">
+                    {this.props.content}
+                </div>
+                <div class="modal-footer">
+                    <a href="#!" class="modal-close waves-effect waves-green btn-flat">{this.props.footerContent}</a>
+                </div>
+            </div>
+        );
+    }
+}
+// ========================================
+var board = ReactDOM.render(<Board />, document.getElementById("materialize"));
+board.insertCard(<Card
+    title="Card Title"
+    content="This is a dummy content that has no specific meaning. Use this as the default content of the card."
+    actions={[
+        <Action content="Here's the 1st link" />,
+        <Action content="Here's the 2nd link" />]} />);
+ReactDOM.render(board.render(), document.getElementById("materialize"));
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.modal');
+    var options = {};
+    var instances = M.Modal.init(elems, options);
+});
